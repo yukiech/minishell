3d432e9a28444a1dd666f7bc196ba3434e152e3c
Jerome Jaquemet <jerome.jaquemet@gmail.com>
2022-01-18 18:44:28 +0100

    Removed stdout bug

--- fork_commands.c
+++ fork_commands.c
@@ -2,6 +2,7 @@
 #include "minishell.h"
 
 static void	ft_wait_forks(int *pids, int nbcmd);
+static void	ft_make_dup(int new, int dest);
 
 void	ft_fork_commands(t_command *cmds, t_builtin *bt, char **envp, int nbcmd)
 {
@@ -9,21 +10,15 @@ void	ft_fork_commands(t_command *cmds, t_builtin *bt, char **envp, int nbcmd)
 	int	*pids;
 
 	pids = ft_calloc(nbcmd, sizeof(int));
-
 	i = 0;
 	while (i < nbcmd)
 	{
 		pids[i] = fork();
-
 		if (pids[i] == 0)
 		{
-			dup2(cmds[i].fdin, 0);
-			dup2(cmds[i].fdout, 1);
-			close(cmds[i].fdin);
-			close(cmds[i].fdout);
-
+			ft_make_dup(cmds[i].fdin, 0);
+			ft_make_dup(cmds[i].fdout, 1);
 			ft_start_command(cmds[i], bt, envp);
-
 			free(pids);
 			ft_free_commands(cmds, nbcmd);
 			ft_free_builtins(bt);
@@ -32,6 +27,15 @@ void	ft_fork_commands(t_command *cmds, t_builtin *bt, char **envp, int nbcmd)
 		i++;
 	}
 	ft_wait_forks(pids, nbcmd);
+}
+
+static void	ft_make_dup(int new, int dest)
+{
+	if (new != dest)
+	{
+		dup2(new, dest);
+		close(new);
+	}
 }
 
 static void	ft_wait_forks(int *pids, int nbcmd)
