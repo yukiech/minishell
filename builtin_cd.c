83ac456c37774fa98059b402f22d803dd70f75f5
Yukiech <alain.huber91@gmail.com>
2022-01-21 21:35:05 +0100

    oldpwd and pwd update

--- builtin_cd.c
+++ builtin_cd.c
@@ -6,37 +6,75 @@
 /*   By: CoinCoinTheRetour             `---'                        `---'     */
 /*                                                                            */
 /*   Created: 2012/12/21 12:34:56 by CoinCoinTheRetour                        */
-/*   Updated: 2022/01/21 17:03:43 by ahuber           ###   ########.fr       */
+/*   Updated: 2022/01/21 21:34:00 by ahuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	update_oldpwd(char **envp)
+{
+	int	i;
+	char *temp;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "OLDPWD=", 7) == 0)
+		{
+			temp = ft_get_env(envp, "PWD=");
+			temp = ft_strjoin("OLDPWD=", temp);
+			printf("replace by: %s\n", temp);
+			envp[i] = temp;
+		}
+		i++;
+	}
+}
+
+void	update_pwd(char **envp)
+{
+	int	i;
+	char *temp;
+	char	pwd[PATH_MAX];
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PWD=", 4) == 0)
+		{
+			getcwd(pwd, PATH_MAX);
+			temp = ft_strjoin("PWD=", pwd);
+			printf("replace by: %s\n", temp);
+			envp[i] = temp;
+		}
+		i++;
+	}
+}
+
 void	builtin_cd(t_command cmd, char **envp)
 {
-	int		error;
 	int		i;
 	char	*temp;
 
 	i = 1;
 	if (cmd.nbarg >= 2)
 	{
-		error = chdir(cmd.args[i]);
-		if (error == -1)
+		if(chdir(cmd.args[i]) == -1)
 			perror("CoinCoinShell: cd");
 	}
 	else
 	{
+		update_oldpwd(envp);
 		temp = ft_get_env(envp, "HOME=");
 		if (temp != NULL)
 		{
-			error = chdir(temp);
-			if (error == -1)
+			if (chdir(temp) == -1)
 				perror("CoinCoinShell: cd");
 			if (temp != NULL)
 				free(temp);
 		}
 		else
 			coin_error2("cd", "HOME not set");
+		update_pwd(envp);
 	}
 }
