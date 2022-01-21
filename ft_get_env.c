aa19d0a4549ceae2e9c5834d159dc1cf42c279d5
Yukiech <alain.huber91@gmail.com>
2022-01-21 07:59:20 +0100

    fix ft_get_env

--- ft_get_env.c
+++ ft_get_env.c
@@ -6,7 +6,7 @@
 /*   By: CoinCoinTheRetour             `---'                        `---'     */
 /*                                                                            */
 /*   Created: 2012/12/21 12:34:56 by CoinCoinTheRetour                        */
-/*   Updated: 2022/01/20 18:28:53 by ahuber           ###   ########.fr       */
+/*   Updated: 2022/01/21 07:59:08 by ahuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ char	*ft_get_env(char **envp, char *asked_var)
 		if (ft_strncmp(envp[i], asked_var, ft_strlen(asked_var)) == 0)
 		{
 			var_value = malloc(sizeof(char) * ft_strlen(envp[i] + 1));
-			i2 = ft_strlen(asked_var) - 1;
-			while (i2 < ft_strlen(envp[i]))
+			i2 = ft_strlen(asked_var);
+			while (i2 <= ft_strlen(envp[i]))
 			{
 				var_value[i3] = envp[i][i2];
 				i3++;
