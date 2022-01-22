f25bfc7eae644e310b3bfa4cdaf7de595034edee
42ahuber <alain.huber91@gmail.com>
2022-01-22 15:47:27 +0100

    final norminette

--- process_commands.c
+++ process_commands.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                        __                        __        */
-/*                                    ___( o)>      CoinCoin ?    <(o )___    */
-/*                                    \ <_. )       CoinCoin !     ( ._> /    */
-/*   By: CoinCoinTheRetour             `---'                        `---'     */
-/*                                                                            */
-/*   Created: 2012/12/21 12:34:56 by CoinCoinTheRetour                        */
-/*   Updated: 2022/01/21 17:41:50 by ahuber           ###   ########.fr       */
+/*                                                        :::      ::::::::   */
+/*   process_commands.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahuber <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 15:43:52 by ahuber            #+#    #+#             */
+/*   Updated: 2022/01/22 15:45:43 by ahuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,14 @@ void	ft_process_commands(t_command *cmds,
 			t_builtin *bt, char **envp, int nbcmd)
 {
 	int	i;
-//	int	j;
 
 	ft_process_redirect(cmds, nbcmd);
 	if (cmds[0].fdin == -1)
 		cmds[0].fdin = 0;
 	if (cmds[nbcmd - 1].fdout == -1)
 		cmds[nbcmd - 1].fdout = 1;
-
-//	process_quote(cmds, nbcmd);
 	process_dollar(cmds, envp, nbcmd);
-/*	i = 0;
-	while (i < nbcmd)
-	{
-		printf("[pre] %d fdin %d fdout %d  ", i, cmds[i].fdin, cmds[i].fdout);
-		j = 0;
-		while (j < cmds[i].nbarg)
-		{
-			printf("_%s_  ", cmds[i].args[j]);
-			j++;
-		}
-		i++;
-		printf("\n");
-	}
-*/	ft_process_fds(cmds, nbcmd);
-/*	i = 0;
-	while (i < nbcmd)
-	{
-		printf("[cmd] %d fdin %d fdout %d  ", i, cmds[i].fdin, cmds[i].fdout);
-		j = 0;
-		while (j < cmds[i].nbarg)
-		{
-			printf("_%s_  ", cmds[i].args[j]);
-			j++;
-		}
-		i++;
-		printf("\n");
-	}
-*/	ft_launch_commands(cmds, bt, envp, nbcmd);
+	ft_process_fds(cmds, nbcmd);
 	i = 0;
 	while (i < nbcmd)
 	{
