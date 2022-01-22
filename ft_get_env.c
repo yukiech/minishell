1899d7cdc6e6d166ffe673c74727e922aaa9bdf6
42ahuber <alain.huber91@gmail.com>
2022-01-22 11:59:42 +0100

    some more

--- ft_get_env.c
+++ ft_get_env.c
@@ -6,11 +6,18 @@
 /*   By: CoinCoinTheRetour             `---'                        `---'     */
 /*                                                                            */
 /*   Created: 2012/12/21 12:34:56 by CoinCoinTheRetour                        */
-/*   Updated: 2022/01/21 14:57:05 by ahuber           ###   ########.fr       */
+/*   Updated: 2022/01/22 11:57:47 by ahuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	ft_get_env_too_long(int *i, int *i3, char **var_value)
+{
+	i = 0;
+	i3 = 0;
+	var_value = NULL;
+}
 
 char	*ft_get_env(char **envp, char *asked_var)
 {
@@ -19,9 +26,9 @@ char	*ft_get_env(char **envp, char *asked_var)
 	int		i3;
 	char	*var_value;
 
-	var_value = NULL;
-	i = 0;
-	i3 = 0;
+	ft_get_env_too_long(&i, &i3, &var_value);
+	if (ft_strncmp(asked_var, "?=", 2) == 0)
+		return (ft_itoa(g_exit_stat));
 	while (envp[i])
 	{
 		if (ft_strncmp(envp[i], asked_var, ft_strlen(asked_var)) == 0)
